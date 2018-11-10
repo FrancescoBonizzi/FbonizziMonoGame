3180e836e157afc028a7db559901723cdb6135df
Francesco Bonizzi <fbonizzi@hotmail.it>
2018-11-10 11:55:30 +0100

    Test

--- FlowsoftGamesMonogame/FlowsoftGamesMonogame/Assets/SplashScreenLoader.cs
+++ FlowsoftGamesMonogame/FlowsoftGamesMonogame/Assets/SplashScreenLoader.cs
@@ -6,7 +6,7 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
-namespace FbonizziMonogame.Assets
+namespace FbonizziMonogame.Assets 
 {
     public class SplashScreenLoader
     {
