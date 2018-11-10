30fc2fe6b962211653ccbbb2d913977b4ecf8966
Francesco Bonizzi <fbonizzi@hotmail.it>
2018-11-10 12:40:13 +0100

    Refactoring

---
+++ FbonizziMonoGameWindowsDesktop/FbonizziMonoGameWindowsDesktop/WindowsTextFileImporter.cs
@@ -0,0 +1,11 @@
+﻿using FbonizziMonogame.Assets;
+using System.IO;
+
+namespace FbonizziMonoGameWindowsDesktop
+{
+    public class WindowsTextFileImporter : ITextFileLoader
+    {
+        public string LoadFile(string filePath)
+          => File.ReadAllText(filePath);
+    }
+}
---
+++ FbonizziMonoGameWindowsDesktop/Playground/Program.cs
@@ -0,0 +1,26 @@
+﻿using FbonizziMonoGameWindowsDesktop;
+using System;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settingsRepository = new WindowsSettingsRepository();
+
+            settingsRepository.GetOrSetBool("test", true);
+            Console.WriteLine(settingsRepository.GetOrSetBool("test", true));
+            settingsRepository.GetOrSetBool("test2", false);
+            Console.WriteLine(settingsRepository.GetOrSetBool("test2", false));
+
+            settingsRepository.GetOrSetTimeSpan("timespan1", TimeSpan.FromSeconds(123));
+            Console.WriteLine(settingsRepository.GetOrSetTimeSpan("timespan1", TimeSpan.FromSeconds(123)));
+
+            settingsRepository.GetOrSetDateTime("data1", DateTime.Now);
+            Console.WriteLine(settingsRepository.GetOrSetDateTime("data1", DateTime.Now));
+
+            Console.Read();
+        }
+    }
+}
